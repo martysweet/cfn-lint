defffdfec1e16773fb029f9cfb95ec9d4ea844d7
RazzM13 <34776591+RazzM13@users.noreply.github.com>
2018-04-03 13:25:11 +0300

    Implemented quick compatibility checking.

--- tools/nodejs_compatibility_checker/test_compatibility_setup.sh
+++ tools/nodejs_compatibility_checker/test_compatibility_setup.sh
@@ -14,10 +14,20 @@ exit 1
 fi
 
 # Determine available node versions
-node_versions=$(nv ls-versions --node-prebuilt | sed 's/\s/\n/g' | grep -E '^[4-9]')
+node_versions=$(nv ls-versions --node-prebuilt | sed 's/\s/\n/g' | grep -E '^[1-9]')
 if [ -z "${node_versions}" ]; then
   echo "Unable to determine node versions!"
   exit 1
+fi
+
+# Quick check
+if [ "$1" == "--quick" ]; then
+  lts_versions=$(
+  for lts in 4 6 8 9; do
+    echo "${node_versions}" | grep -E "^${lts}" | tail -1
+  done
+  )
+  node_versions=${lts_versions}
 fi
 
 # Dump available versions to file
