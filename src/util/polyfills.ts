c32d77a54f4aed2e901768fd1f08b66cc1a22346
Razvan Muscalu <34776591+RazzM13@users.noreply.github.com>
2018-04-04 15:37:53 +0300

    Replace "safer-buffer" polyfill with "safe-buffer". (#130)
    * Implemented "safer-buffer" polyfill.
    
    * Swapped "safer-buffer" for "safe-buffer" polyfill.
    
    * Change safer-buffer to safe-buffer

--- src/util/polyfills.ts
+++ src/util/polyfills.ts
@@ -1,4 +1,4 @@
 require('core-js/fn/string/raw');
 require('core-js/fn/object/assign');
 require('core-js/fn/array/entries');
-Buffer = require('safer-buffer').Buffer;
+Buffer = require('safe-buffer').Buffer;
