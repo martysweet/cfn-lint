2a8d9b632708d71ca4aa2d99f4e285e97f3db482
RazzM13 <34776591+RazzM13@users.noreply.github.com>
2018-04-20 21:06:21 +0300

    Fixed coding style for "index.ts"

--- src/index.ts
+++ src/index.ts
@@ -17,134 +17,134 @@ function list(val: string) {
 }
 
 function doNoCommand() {
-    console.error('\nno command given!');
+    console.error('\n', 'no command given!');
     process.exit(1);
 }
 
 function doNoArgument() {
-    console.error('\nmissing required argument');
+    console.error('\n', 'missing required argument');
     process.exit(1);
 }
 
 program
-.version(version)
-.action(function() {
-    program.help();
-    doNoCommand();
-})
-.on('--help', function(){
-    doNoCommand();
-});
+    .version(version)
+    .action(function() {
+        program.help();
+        doNoCommand();
+    })
+    .on('--help', function() {
+        doNoCommand();
+    });
 
 program
-.command('validate')
-.option('-p, --parameters <items>', 'List of params', list)
-.option('-p, --pseudo <items>', 'List of pseudo overrides', list)
-.option('--guess-parameters', 'Guess any parameters that are not explicitely passed in and have no Default. This is the default behaviour.')
-.option('-G, --no-guess-parameters', 'Fail validation if a parameter with no Default is not passed')
-.option('-g, --only-guess-parameters <items>', 'Guess the provided parameters, and fail validation if a parameter with no Default is passed', list)
-.option('-v, --verbose', 'Verbose error messages')
-.action(function(file, cmd) {
-    // Patch for CommanderJS bug that defaults this to true
-    if (cmd.parent.rawArgs.indexOf('--guess-parameters') != -1) {
-      cmd.guessParameters = true;
-    }
-
-    const validator = require('./validator') as typeof validatorBaseImport;
-    if(cmd.parameters){
-        for(let param of cmd.parameters){
-            // Set the parameter
-            let kv = param.split('=');
-            validator.addParameterValue(kv[0], kv[1]);
+    .command('validate')
+    .option('-p, --parameters <items>', 'List of params', list)
+    .option('-p, --pseudo <items>', 'List of pseudo overrides', list)
+    .option('--guess-parameters', 'Guess any parameters that are not explicitely passed in and have no Default. This is the default behaviour.')
+    .option('-G, --no-guess-parameters', 'Fail validation if a parameter with no Default is not passed')
+    .option('-g, --only-guess-parameters <items>', 'Guess the provided parameters, and fail validation if a parameter with no Default is passed', list)
+    .option('-v, --verbose', 'Verbose error messages')
+    .action(function(file, cmd) {
+        // Patch for CommanderJS bug that defaults this to true
+        if (cmd.parent.rawArgs.indexOf('--guess-parameters') != -1) {
+          cmd.guessParameters = true;
         }
-    }
 
-    if(cmd.pseudo){
-        for(let pseudo of cmd.pseudo){
-            // Set the parameter
-            let kv = pseudo.split('=');
-            validator.addPseudoValue(kv[0], kv[1]);
-        }
-    }
-
-    let guessParameters: string[] | undefined;
-    if (cmd.guessParameters === false) {
-        guessParameters = [];
-    } else if (cmd.onlyGuessParameters) {
-        guessParameters = cmd.onlyGuessParameters;
-    } else {
-        guessParameters = undefined;
-    }
-
-    const options = {
-        guessParameters
-    };
-
-    let result = Object();
-    try {
-        result = validator.validateFile(file, options);
-    } catch(err) {
-        let error: string = function(msg: string, errors: any) {
-            for (let error of Object.keys(errors)) {
-                if (RegExp(error).test(msg)) {
-                    return errors[error];
-                }
+        const validator = require('./validator') as typeof validatorBaseImport;
+        if(cmd.parameters) {
+            for(let param of cmd.parameters) {
+                // Set the parameter
+                let kv = param.split('=');
+                validator.addParameterValue(kv[0], kv[1]);
             }
-            return errors[''];
-        }(err.message, {
-            'Could not find file .*. Check the input path.': 'No such file.',
-            '': 'Unable to parse template! Use --verbose for more information.'
-        });
-        console.log(error);
-        if (cmd.verbose) {
-            console.error(err);
         }
-        process.exit(1);
-    }
 
-    // Show the errors
-    console.log((result['errors']['info'].length + " infos").grey);
-    for(let info of result['errors']['info']){
-        console.log('Resource: '+ info['resource'].grey);
-        console.log('Message: '+ info['message'].grey);
-        console.log('Documentation: '+ info['documentation'].grey + '\n');
-    }
+        if(cmd.pseudo) {
+            for(let pseudo of cmd.pseudo) {
+                // Set the parameter
+                let kv = pseudo.split('=');
+                validator.addPseudoValue(kv[0], kv[1]);
+            }
+        }
 
-    console.log((result['errors']['warn'].length + " warn").yellow);
-    for(let warn of result['errors']['warn']){
-        console.log('Resource: ' + warn['resource'].yellow);
-        console.log('Message: ' + warn['message'].yellow);
-        console.log('Documentation: ' + warn['documentation'].yellow + '\n');
-    }
+        let guessParameters: string[] | undefined;
+        if (cmd.guessParameters === false) {
+            guessParameters = [];
+        } else if (cmd.onlyGuessParameters) {
+            guessParameters = cmd.onlyGuessParameters;
+        } else {
+            guessParameters = undefined;
+        }
 
-    console.log((result['errors']['crit'].length + " crit").red);
-    for(let crit of result['errors']['crit']){
-        console.log('Resource: ' + crit['resource'].red);
-        console.log('Message: ' + crit['message'].red);
-        console.log('Documentation: ' + crit['documentation'].red + '\n');
-    }
+        const options = {
+            guessParameters
+        };
 
-    if(result['templateValid'] === false){
-        console.log('Template invalid!'.red.bold);
-        process.exit(1);
-    }else{
-        console.log('Template valid!'.green);
-        process.exit(0);
-    }
-})
-.on('--help', function(){
-    doNoArgument();
-});
+        let result = Object();
+        try {
+            result = validator.validateFile(file, options);
+        } catch(err) {
+            let error: string = function(msg: string, errors: any) {
+                for (let error of Object.keys(errors)) {
+                    if (RegExp(error).test(msg)) {
+                        return errors[error];
+                    }
+                }
+                return errors[''];
+            }(err.message, {
+                'Could not find file .*. Check the input path.': 'No such file.',
+                '': 'Unable to parse template! Use --verbose for more information.'
+            });
+            console.log(error);
+            if (cmd.verbose) {
+                console.error(err);
+            }
+            process.exit(1);
+        }
+
+        // Show the errors
+        console.log((result['errors']['info'].length + " infos").grey);
+        for(let info of result['errors']['info']) {
+            console.log('Resource: '+ info['resource'].grey);
+            console.log('Message: '+ info['message'].grey);
+            console.log('Documentation: '+ info['documentation'].grey + '\n');
+        }
+
+        console.log((result['errors']['warn'].length + " warn").yellow);
+        for(let warn of result['errors']['warn']) {
+            console.log('Resource: ' + warn['resource'].yellow);
+            console.log('Message: ' + warn['message'].yellow);
+            console.log('Documentation: ' + warn['documentation'].yellow + '\n');
+        }
+
+        console.log((result['errors']['crit'].length + " crit").red);
+        for(let crit of result['errors']['crit']) {
+            console.log('Resource: ' + crit['resource'].red);
+            console.log('Message: ' + crit['message'].red);
+            console.log('Documentation: ' + crit['documentation'].red + '\n');
+        }
+
+        if(result['templateValid'] === false) {
+            console.log('Template invalid!'.red.bold);
+            process.exit(1);
+        } else {
+            console.log('Template valid!'.green);
+            process.exit(0);
+        }
+    })
+    .on('--help', function() {
+        doNoArgument();
+    });
 
 program
-.command('docs')
-.action(function(reference) {
-    const docs = require('./docs') as typeof docsBaseImport;
-    console.log(docs.getDoc(reference));
-})
-.on('--help', function(){
-    doNoArgument();
-});
+    .command('docs')
+    .action(function(reference) {
+        const docs = require('./docs') as typeof docsBaseImport;
+        console.log(docs.getDoc(reference));
+    })
+    .on('--help', function() {
+        doNoArgument();
+    });
 
 if (process.argv.length < 4) {
     process.argv.push('--help');
