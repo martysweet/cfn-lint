7975480ab9d77bef377b47aef805a584a5ebd2d5
Marty Sweet <martysweet@users.noreply.github.com>
2019-02-27 16:21:53 +0000

    Fix Ref ARN Return types (#226)
    * Fix arn issue caused by type parameterization
    
    * Add additional resources with ref return types
    
    * Remove console log
    
    * Update CHANGELOG.md

--- src/test/validatorTest.ts
+++ src/test/validatorTest.ts
@@ -931,6 +931,13 @@ describe('validator', () => {
             expect(result).to.have.deep.property('templateValid', true);
             expect(result['errors']['crit']).to.have.lengthOf(0);
         });
+
+        it('Issue #212 - Ref does not account for resources that return ARNS', function () {
+            var input = 'testData/valid/yaml/issue-212-ref-arn.yaml';
+            var result = validator.validateFile(input);
+            expect(result).to.have.deep.property('templateValid', true);
+            expect(result['errors']['crit']).to.have.lengthOf(0);
+        });
     });
 
     describe('parameters-validation', () => {
